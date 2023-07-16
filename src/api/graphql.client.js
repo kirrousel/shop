25586eb573d52d85272690b4fd582e358d19b355
kirrousel <kirrousel@gmail.com>
2023-07-16 14:33:44 +0200

    chore: update graphql client

--- src/api/graphql.client.js
+++ src/api/graphql.client.js
@@ -1,30 +1,12 @@
-import {ApolloClient, ApolloLink, createHttpLink, from, InMemoryCache} from '@apollo/client/core'
+import {ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client/core'
 
 // HTTP connection to the API
 const httpLink = createHttpLink({
     uri: 'https://venia.magento.com/graphql',
-    fetchOptions: {
-        mode: 'no-cors'
-    }
 })
 
 // Cache implementation
 const cache = new InMemoryCache()
-
-const authMiddleware = new ApolloLink((operation, forward) => {
-    operation.setContext({
-        headers: {
-            'Access-Control-Allow-Credentials': true,
-            'Access-Control-Allow-Origin': '*',
-            "Content-Type": 'application/json',
-            'Apollo-Require-Preflight': false,
-        },
-        fetchOptions: {
-            mode: 'no-cors'
-        }
-    });
-    return forward(operation);
-});
 
 // Create the apollo client
 export const apolloClient = new ApolloClient({
